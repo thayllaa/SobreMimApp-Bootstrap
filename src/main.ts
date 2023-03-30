dbd79e370922ccb77b5cd79b5d2fd05746094786
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-03-29 21:52:31 -0300

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule],
-  template: ``,
-})
-export class App {}
-
-bootstrapApplication(App);
+platformBrowserDynamic() 
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
